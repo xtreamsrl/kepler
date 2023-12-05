fix: fix mass in derivatives computation and add realistic data for planets

src/solar_system.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 from src.body import Body
 
-G = 6.67e-20  # Gravitational constant (km**3/kg/s**2)
+G = 6.67e-11  # Gravitational constant (m**3/kg/s**2)
 
 
 class SolarSystem:
@@ -39,15 +39,14 @@ class SolarSystem:
         derivatives of the equations of motion describing the n-body system
         t is unused
         """
+        masses = [planet.mass for planet in self.planets]
         derivatives = []
         for i in range(_y.shape[0]):
             ri = _y[i, 0:3]
-            mi = self.planets[i].mass
             vi = _y[i, 3:6]
-
             # acceleration
-            ai = G * mi * sum([
-                (_y[j, 0:3] - ri) / np.linalg.norm(_y[j, 0:3] - ri) ** 3
+            ai = G * sum([
+                masses[j] * (_y[j, 0:3] - ri) / np.linalg.norm(_y[j, 0:3] - ri) ** 3
                 for j in set(range(_y.shape[0])) - {i}
             ])
             derivatives.append(np.concatenate([vi, ai]))

src/visualization.py
@@ -87,9 +87,8 @@ def plot_animation(bodies: list[Body]):
     plt.show()
 
 
-def orbit_plot(bodies):
+def plot_orbits(bodies):
     trajectories = np.stack([b.states for b in bodies]).swapaxes(1, 2)
-    masses = [b.mass for b in bodies]
     plt.style.use('dark_background')
     colors = mpl.colormaps["Set3"].colors
 